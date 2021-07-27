38d7c95a7d0f2658e34305dc07d7cefc08632b7b
Thomas Thompson <tomthompson828@gmail.com>
2021-07-27 00:25:02 -0400

    Feature: Add Static and Template folders

--- app/app.py
+++ app/app.py
@@ -5,6 +5,15 @@ from flask import Flask
 app = Flask(__name__)
 
 
+# Create Flask's `app` object
+app = Flask(
+    __name__,
+    instance_relative_config=False,
+    template_folder="templates",
+    static_folder="static"
+)
+
+
 @app.route("/")
 def hello():
     return "Hello World!"
