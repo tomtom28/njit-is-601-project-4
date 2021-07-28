762ee0f76678d312f783d6f0472f15fc3f15bfe3
Thomas Thompson <tomthompson828@gmail.com>
2021-07-27 23:35:49 -0400

    Feature: Working Part ii on port 5001

---
+++ flask-jinja-tutorial/flask_jinja_tutorial/routes.py
@@ -0,0 +1,22 @@
+"""Route declaration."""
+from flask import Flask
+from flask import current_app as app
+from flask import render_template
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    """Landing page."""
+    return "hello"
+    # return render_template(
+    #     'home.html',
+    #     title="Jinja Demo Site",
+    #     description="Smarter page templates with Flask & Jinja."
+    # )
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5001, debug=True)
