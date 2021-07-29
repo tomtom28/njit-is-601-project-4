24c93d9966c1de7b65a65bcf3b6d621e8ac62b50
Thomas Thompson <tomthompson828@gmail.com>
2021-07-29 00:12:16 -0400

    Feature: Part 4 - Dynamic Routes & Variable Rules

--- app4/app.py
+++ app4/app.py
@@ -13,5 +13,17 @@ def users():
     return "This API endpoint accepts GET, POST, or PUT requests"
 
 
+@app.route('/user/<username>')
+def profile(username):
+    # ... Logic goes here
+    return "Hello, " + str(username)
+
+
+@app.route('/<int:year>/<int:month>/<title>')
+def article(year, month, title):
+    # ... Logic goes here
+    return "Article Title: " + str(title) + "<br/> Month: " + str(month) + "<br/> Year: " + str(year)
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5003, debug=True)
