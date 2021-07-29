fbf0f5cbe770e560a431a2979b0353074b26fef6
Thomas Thompson <tomthompson828@gmail.com>
2021-07-29 00:04:34 -0400

    Feature: Part 4 - Route HTTP Methods

--- app4/app.py
+++ app4/app.py
@@ -7,5 +7,11 @@ def home():
     return "Hello World!"
 
 
+@app.route("/api/v1/users/", methods=['GET', 'POST', 'PUT'])
+def users():
+    # ... Logic goes here
+    return "This API endpoint accepts GET, POST, or PUT requests"
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5003, debug=True)
