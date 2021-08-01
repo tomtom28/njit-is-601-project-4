eaea5ea986312ed9dfa1b6ea57625e9c6ded7994
Thomas Thompson <tomthompson828@gmail.com>
2021-07-31 21:52:18 -0400

    Feature: Part 9 - Working App Basic Test

---
+++ flask-sqlalchemy-tutorial/flask_sqlalchemy_tutorial/routes.py
@@ -0,0 +1,23 @@
+"""Initialize Flask app."""
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from ddtrace import patch_all
+
+app = Flask(__name__, instance_relative_config=False)
+app.config.from_object("config.Config")
+
+db = SQLAlchemy()
+# db.init_app(app)
+# db.create_all()  # Create database tables for our data models
+
+
+patch_all()
+
+
+@app.route("/", methods=["GET"])
+def index():
+    return "hello"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5008, debug=True)
